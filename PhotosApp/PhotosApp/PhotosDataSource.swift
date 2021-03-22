138850946cbc2c5298ecfc809405c22ba67bf807
Lia <judykim257@gmail.com>
2021-03-22 18:03:08 +0900

    Photo DataSource 분리

--- PhotosApp/PhotosApp/PhotosDataSource.swift
+++ PhotosApp/PhotosApp/PhotosDataSource.swift
@@ -2,7 +2,37 @@
 //  PhotosDataSource.swift
 //  PhotosApp
 //
-//  Created by 김지경 on 2021/03/22.
+//  Created by Lia on 2021/03/22.
 //
 
-import Foundation
+import UIKit
+
+class PhotosDataSource: NSObject {
+    private let photos: [Photo]
+    
+    init(photos: [Photo]) {
+        self.photos = photos
+    }
+    
+    func getPhoto(at indexPath: IndexPath) -> Photo {
+        return photos[indexPath.item]
+    }
+}
+
+extension PhotosDataSource : UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return photos.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCell", for: indexPath) as! PhotoCell
+        let photo = getPhoto(at: indexPath)
+        
+        cell.imageView.image = UIImage(named: photo.thumbnail)
+        cell.imageView.backgroundColor = UIColor.randomColor()
+        
+        return cell
+    }
+}
+
--- PhotosApp/PhotosApp/ViewController.swift
+++ PhotosApp/PhotosApp/ViewController.swift
@@ -10,30 +10,16 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var collectionView: UICollectionView!
+    private var photosDataSource: PhotosDataSource!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        let photos = PhotosGenerator.generate()
+        photosDataSource = PhotosDataSource(photos: photos)
+        self.collectionView.dataSource = photosDataSource
         
-        self.collectionView.delegate = self
-        self.collectionView.dataSource = self
     }
 
 
-}
-
-extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 40
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCell", for: indexPath) as! PhotoCell
-        
-        cell.imageView.backgroundColor = UIColor.randomColor()
-        
-        return cell
-    }
-    
-    
 }
