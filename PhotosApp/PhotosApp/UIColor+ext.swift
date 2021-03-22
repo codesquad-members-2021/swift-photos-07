43d764d21e800a8c50ec096e7f369849abd618b7
Lia <judykim257@gmail.com>
2021-03-22 15:51:36 +0900

    randomColor 구현

--- PhotosApp/PhotosApp/UIColor+ext.swift
+++ PhotosApp/PhotosApp/UIColor+ext.swift
@@ -2,14 +2,20 @@
 //  UIColor+ext.swift
 //  PhotosApp
 //
-//  Created by 김지경 on 2021/03/22.
+//  Created by Lia on 2021/03/22.
 //
 
 import Foundation
 import UIKit
 
 extension UIColor {
-    static func randomColor() {
+    static func randomColor() -> UIColor {
+        let redLevel = CGFloat.random(in: 0...1)
+        let greenLevel = CGFloat.random(in: 0...1)
+        let blueLevel = CGFloat.random(in: 0...1)
+
+        let color = UIColor(red: redLevel, green: greenLevel, blue: blueLevel, alpha: 1)
         
+        return color
     }
 }
--- PhotosApp/PhotosApp/ViewController.swift
+++ PhotosApp/PhotosApp/ViewController.swift
@@ -10,22 +10,29 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        collectionView.dataSource = self
+        
+        self.collectionView.delegate = self
+        self.collectionView.dataSource = self
     }
 
 
 }
 
-extension ViewController: UICollectionViewDataSource {
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 40
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = 
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCell", for: indexPath) as! PhotoCell
+        
+        cell.backgroundColor = UIColor.randomColor()
+        
+        return cell
     }
     
     
