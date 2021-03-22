9661abdc91eb39e81a4bce4759d40867d2ec2df6
Lia <judykim257@gmail.com>
2021-03-22 18:01:31 +0900

    Photo model 생성

---
+++ PhotosApp/PhotosApp/Photo.swift
@@ -0,0 +1,16 @@
+//
+//  Photo.swift
+//  PhotosApp
+//
+//  Created by Lia on 2021/03/22.
+//
+
+import Foundation
+
+struct Photo {
+    
+    let thumbnail: String
+    
+    
+}
+
---
+++ PhotosApp/PhotosApp/PhotosDataSource.swift
@@ -0,0 +1,8 @@
+//
+//  PhotosDataSource.swift
+//  PhotosApp
+//
+//  Created by 김지경 on 2021/03/22.
+//
+
+import Foundation
