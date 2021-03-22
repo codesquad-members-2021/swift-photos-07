4f12a099a6721cce04695600a9a5870ea744c6ef
Lia <judykim257@gmail.com>
2021-03-22 18:03:55 +0900

    Dummy Photos 생성자 생성

--- PhotosApp/PhotosApp/PhotosGenerator.swift
+++ PhotosApp/PhotosApp/PhotosGenerator.swift
@@ -2,7 +2,21 @@
 //  PhotosGenerator.swift
 //  PhotosApp
 //
-//  Created by 김지경 on 2021/03/22.
+//  Created by Lia on 2021/03/22.
 //
 
 import Foundation
+
+class PhotosGenerator {
+    
+    class func generate() -> [Photo] {
+        
+        var photoList = [Photo]()
+        
+        for _ in 0..<40 {
+            photoList.append(Photo(thumbnail: ""))
+        }
+        
+        return photoList
+    }
+}
