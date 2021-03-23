55bb72159f65a01eb8dded1b753697a8a723d290
zombietux <>
2021-03-23 12:09:44 +0900

    PHAsset을 UIImage로 변환하는 코드 추가

--- PhotosApp/PhotosApp/PHAsset+ext.swift
+++ PhotosApp/PhotosApp/PHAsset+ext.swift
@@ -5,4 +5,16 @@
 //  Created by zombietux on 2021/03/23.
 //
 
-import Foundation
+import UIKit
+import Photos
+
+extension PHAsset {
+    var image : UIImage {
+        var thumbnail = UIImage()
+        let imageManager = PHCachingImageManager()
+        imageManager.requestImage(for: self, targetSize: CGSize(width: 100, height: 100), contentMode: .aspectFit, options: nil, resultHandler: { image, _ in
+            thumbnail = image!
+        })
+        return thumbnail
+    }
+}
