c56de288b79e226831f291fdbd1bf873170c8e7c
Lia <judykim257@gmail.com>
2021-03-23 15:05:10 +0900

    step3 - DoodleViewController, navigation controller embed

---
+++ PhotosApp/PhotosApp/DoodleViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DoodleViewController.swift
+//  PhotosApp
+//
+//  Created by Lia on 2021/03/23.
+//
+
+import UIKit
+
+class DoodleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+
+}
