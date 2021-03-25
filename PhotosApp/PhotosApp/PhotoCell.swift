8fbad65040f4f77a704fac9c7dc21e4c3ddd22a9
zombietux <>
2021-03-25 18:23:17 +0900

    cell selected 수정

--- PhotosApp/PhotosApp/PhotoCell.swift
+++ PhotosApp/PhotosApp/PhotoCell.swift
@@ -18,7 +18,9 @@ class PhotoCell: UICollectionViewCell {
     
     override var isSelected: Bool {
         didSet {
-            self.selectedBackgroundView?.transform = CGAffineTransform(scaleX: 0.95, y: 0.95)
+            let backgroundView = UIView()
+            backgroundView.backgroundColor = UIColor.red
+            self.selectedBackgroundView = backgroundView
         }
     }
     
@@ -30,24 +32,5 @@ class PhotoCell: UICollectionViewCell {
         super.prepareForReuse()
         imageView.image = nil
         self.selectedBackgroundView?.transform = CGAffineTransform.identity
-    }
-    
-    override var isSelected: Bool {
-        didSet {
-            if isSelected {
-                print("👍🏻")
-                
-                let backgroundView = UIView()
-                backgroundView.backgroundColor = UIColor.red
-                self.selectedBackgroundView = backgroundView
-                
-                self.imageView.transform = CGAffineTransform(scaleX: 0.95, y: 0.95)
-                self.imageView.alpha = 1
-            } else {
-                self.selectedBackgroundView?.backgroundColor = UIColor.blue
-                self.imageView.transform = CGAffineTransform(scaleX: 1.05, y: 1.05)
-                print("else")
-            }
-        }
     }
 }
